83d98a737157fbd9529338cb63aeb22f4e76cbde
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 19:23:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,5 +46,5 @@ end
 def checkout(cart, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart,coupons)
-  applied_discount = apply_clearance
+  applied_discount = apply_clearance(applied_coupons)
 end
