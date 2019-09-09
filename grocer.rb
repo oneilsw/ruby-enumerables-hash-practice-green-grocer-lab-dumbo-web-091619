0586abc01c574cc4df6efbe652131315e6325ecd
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 18:05:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,6 +25,7 @@ def apply_coupons(cart, coupons)
     if cart[item] && coupon[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[:item][:clearance], count: coupon[:num] }
       cart[:item][:count] -= coupon[:num]
+    elseif 
         binding.pry
     end 
   end 
