cb026b92027c1d035d0f2ce93538a7929592d900
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 17:50:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]  
-    if cart[item] && 
+    if cart[item] && coupon[:item]
         binding.pry
     end 
   end 
