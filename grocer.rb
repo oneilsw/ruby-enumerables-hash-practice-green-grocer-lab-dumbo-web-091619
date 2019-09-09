bcade12dcd25b5ebc591294ac01179bfc96e5e2f
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 19:46:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,5 +48,5 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(hash_cart,coupons)
   applied_discount = apply_clearance(applied_coupons)
   total =applied_discount.reduce(0) {|acc,(key,value)| acc += value[:price] * value[:count]} 
-  total > 100 ? total 
+  total > 100 ? total * 0.9
 end
