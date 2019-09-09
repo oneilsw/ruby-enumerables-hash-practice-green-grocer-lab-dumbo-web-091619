4876cb6d65a4c7b165c04deb6095c9223af6e772
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 18:36:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def apply_coupons(cart, coupons)
        cart["#{item} W/COUPON"][:count] += coupon[:num]
       end 
       cart[item][:count] -= coupon[:num]
-    end 
+    end  
   end 
  cart 
 end
