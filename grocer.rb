67ceee80a05c5d9d0b6b7df397e95db412ac7018
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 17:53:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]  
-    if cart[item] && coupon[:item][:count] >= coupon[:num] && cart["#{item} w/"]
+    if cart[item] && coupon[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
         binding.pry
     end 
   end 
