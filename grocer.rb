ce47dee15617186d45df8b1c4bbdc17c84258370
Oneil Williams <oneilswilliams@gmail.com>
2019-09-09 18:50:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,7 +27,7 @@ def apply_coupons(cart, coupons)
         if !cart[coupon_item]
           cart[coupon_item] = {count: coupon[:num], price: coupon[:cost]/coupon[:num]
         else 
-          cart
+          cart[coupon_item]
     end
   end 
  cart 
